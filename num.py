7cd142db4847f306d55f097e12eb63807731596f
thisissolmi <oksk6685@naver.com>
2024-01-06 23:11:31 +0900

    기본 연산

---
+++ num.py
@@ -0,0 +1,10 @@
+#floor division (버림 나눗셈)
+print(7//2)
+print(8//3)
+print(8.0//3) #하나라도 floating 이 있으면 결과가 floating 이다
+
+#round (반올림) 안에 있는 숫자를 반올림 ->
+print(round(3.1415926535))
+
+#얘같은경우에는 반올림을 소숫점 둘쨌자리까지
+print(round(3.1415926535,2))
