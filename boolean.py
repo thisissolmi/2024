11f4765c910f281fd06b2b98646190037467d22c
thisissolmi <oksk6685@naver.com>
2024-01-09 23:47:42 +0900

    boolean

---
+++ boolean.py
@@ -0,0 +1,16 @@
+#불린
+print(True)
+print(False)
+
+print("and 연산자")
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+
+
+print("or 연산자")
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
