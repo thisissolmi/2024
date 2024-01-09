edfc85bcaa36646d60a0ef016ef34c4632dc25a7
thisissolmi <oksk6685@naver.com>
2024-01-09 23:51:18 +0900

    0109

--- boolean.py
+++ boolean.py
@@ -14,3 +14,13 @@ print(True or True)
 print(True or False)
 print(False or True)
 print(False or False)
+
+print("not 반대로 출력")
+print(not True)
+print(not False)
+
+print("기본 연산")
+print(2>1)
+
+print("문자열 T/F")
+print("hello" == "hello")
