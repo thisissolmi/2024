5e3ef212174f10ff49d8b6d263ea8290ef090414
thisissolmi <oksk6685@naver.com>
2024-01-09 23:59:19 +0900

    test

--- python/type_function.py
+++ python/type_function.py
@@ -10,3 +10,26 @@ def hello():
 
 print(type(hello))
 print(type(print))
+
+print(int(2.5) + int(3.8) > int(str(1) + str(2)))
+print(True and (True or False))
+print(not True or (True and False))
+
+print(False == False)
+
+
+print(type(2 * 3 == 6))
+print(type(10 <= 7))
+
+print(type("True"))
+
+print("--")
+print(type(4 / 2))
+print(type("True"))
+print(type(10 <= 7))
+print(type(2.0 ** 3))
+print(type(2 * 3 == 6))
+
+
+
+
